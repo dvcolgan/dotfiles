9070f53af1e6e78cdf8b47c215512c6b6ce9a321
David Colgan <david@recursivegarden.com>
2025-03-16 14:22:54 -0400

    add python-dotenv to settings

--- api/settings.py
+++ api/settings.py
@@ -1,5 +1,30 @@
+import logging
+import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent
+from dotenv import load_dotenv
 
-DEBUG = True
+logger = logging.getLogger(__name__)
+
+BASE_DIR = Path(__file__).parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+
+def get_env(key, default=None, as_type=str):
+    value = os.getenv(key, None)
+    if value is None:
+        if default is not None:
+            value = default
+        else:
+            raise ValueError(f"""
+Missing required variable in .env file:
+
+{key}
+
+Please make sure your .env file is correct.""")
+    return as_type(value)
+
+
+DEBUG = get_env("DEBUG", default=False)
+
+ANTHROPIC_API_KEY: str = get_env("ANTHROPIC_API_KEY")
